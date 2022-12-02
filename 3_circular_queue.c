8b11ae1e72a8a381b13632d690239b7046df71ae
Nishant Bhat <97377851+highlyreactiveNeon@users.noreply.github.com>
2022-12-02 23:56:08 +0530

    Update 3_circular_queue.c

--- 3_circular_queue.c
+++ 3_circular_queue.c
@@ -1,38 +1,83 @@
 #include <stdio.h>
-#include <stdlib.h>
+# define SIZE 100
 
-typedef struct QueueInt {
-    size_t head;
-    size_t size;    //rear
-    size_t count;
-    size_t capacity;
-    int* container;
-} QueueInt;
-
-void queueIntConstructor(QueueInt* q) {
-    q->head = 0;
-    q->size = 0;
-    q->capacity = 1;
-    q->container = (int*)malloc(sizeof(int)*1);
-}
-
-void enqueueInt(QueueInt* q, int ele) {
-    if(size == capacity) {
-        q->container = realloc(q->container, capacity*2);
-        q->capacity *= 2;
+void push();
+void pop();
+void show();
+int inp_arr[SIZE];
+int Rear = - 1;
+int Front = - 1;
+main()
+{
+    int ch;
+    while (1)
+    {
+        printf("1.Push\n");
+        printf("2.Pop\n");
+        printf("3.Display\n");
+        printf("4.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                show();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Incorrect choice \n");
+        } 
+    } 
+} 
+ 
+void push()
+{
+    int insert_item;
+    if (Rear == SIZE - 1)
+       printf("Overflow \n");
+    else
+    {
+        if (Front == - 1)
+      
+        Front = 0;
+        printf("Element to be inserted in the Queue\n : ");
+        scanf("%d", &insert_item);
+        Rear = Rear + 1;
+        inp_arr[Rear] = insert_item;
     }
-    
-    q->container[max] = ele;
-    q->size++;
-}
-
-int dequeueInt(QueueInt* q) {
-    q->head
-}
-
-int main()
+} 
+ 
+void pop()
+{
+    if (Front == - 1 || Front > Rear)
+    {
+        printf("Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from the Queue: %d\n", inp_arr[Front]);
+        Front = Front + 1;
+    }
+} 
+ 
+void show()
 {
     
-
-    return 0;
+    if (Front == - 1)
+        printf("Empty Queue \n");
+    else
+    {
+        printf("Queue: \n");
+        for (int i = Front; i <= Rear; i++)
+            printf("%d ", inp_arr[i]);
+        printf("\n");
+    }
 }
