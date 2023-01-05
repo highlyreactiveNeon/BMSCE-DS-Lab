27583df146b41bf083ef04b2895f50fcb06e2398
highlyreactiveNeon <unravellingneon@outlook.com>
2023-01-05 21:12:02 +0530

    added a interface to linked_list

--- 4_linkedlist.c
+++ 4_linkedlist.c
@@ -1,191 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-typedef struct Node {
-    struct Node* nextNode;
-    int data;
-} Node;
-
-typedef struct List {
-    struct Node* head;
-    int size;
-} List;
-
-void listConstructor(List* list) {
-    list->size = 0;
-    list->head = (Node*)0;
-}
-
-void listConstructorHeap(List** list) {
-    *list = (List*)malloc(sizeof(List));
-    (*list)->size = 0;
-    (*list)->head = (Node*)0;
-}
-
-void push_front(List* list, int val) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = val;
-    newNode->nextNode = list->head;
-
-    list->head = newNode;
-    list->size++;
-}
-
-// void push_back(List* list, int val) {
-//     Node* currentNode = list->head;
-//     Node* prevNode = (Node*)list;
-//     while(currentNode != (Node*)0){
-//         prevNode = currentNode;
-//         currentNode = currentNode->nextNode;
-//     }
-//     currentNode = (Node*)malloc(sizeof(Node));
-//     prevNode->nextNode = currentNode;
-//     currentNode->data = val;
-//     currentNode->nextNode = (Node*)0;
-//     list->size++;
-// }
-
-void push_back(List* list, int val) {
-    Node* currentNode = (Node*)list;
-    while(currentNode->nextNode != (Node*)0)
-        currentNode = currentNode->nextNode;
-    currentNode->nextNode = (Node*)malloc(sizeof(Node));
-    currentNode = currentNode->nextNode;
-    currentNode->data = val;
-    currentNode->nextNode = (Node*)0;
-    list->size++;
-}
-
-void push_at(List* list, int pos, int val) {
-    Node* currentNode = (Node*)list;
-
-    for(int i = 0; i < pos; i++)
-        currentNode = currentNode->nextNode;
-
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = val;
-    newNode->nextNode = currentNode->nextNode;
-    currentNode->nextNode = newNode;
-}
-
-int at(List* list, int pos) {
-    Node* currentNode = list->head;
-
-    for(int i = 0; i < pos; i++)
-        currentNode = currentNode->nextNode;
-
-    return currentNode->data;
-}
-
-int front(List* list) {
-    return list->head->data;
-}
-
-int back(List* list) {
-    Node* currentNode = (Node*)list;
-
-    while(currentNode->nextNode != (Node*)0)
-        currentNode = currentNode->nextNode;
-
-    return currentNode->data;    
-}
-
-void pop_front(List* list) {
-    Node* temp = list->head;
-    list->head = temp->nextNode;
-    free(temp);
-}
-
-void pop_back(List* list) {
-    Node* currentNode = list->head;
-    Node* prevNode = (Node*)list;
-
-    while(currentNode->nextNode != (Node*)0) {
-        prevNode = currentNode;
-        currentNode = currentNode->nextNode;
-    }
-
-    prevNode->nextNode = (Node*)0;
-    free(currentNode);
-}
-
-void pop_at(List* list, int pos) {
-    Node* currentNode = list->head;
-    Node* prevNode = (Node*)list;
-
-    for(int i = 0; i < pos; i++) {
-        prevNode = currentNode;
-        currentNode = currentNode->nextNode;
-    }
-
-    prevNode->nextNode = currentNode->nextNode;
-    free(currentNode);
-}
-
-void print(List* list) {
-    if(list->size == 0){
-        printf("The list is empty\n");
-        return;
-    }
-
-    Node* currentNode = list->head;
-
-    printf("The contents of the list are: ");
-    while(currentNode != (Node*)0) {
-        printf("%d ", currentNode->data);
-        currentNode = currentNode->nextNode;
-    }
-    printf("\n");
-}
+#include <stdio.h>
+#include "list.h"
 
 int main() {
-    // List list;
-    // listConstructor(&list);
-
-    // push_back(&list, 6);
-    // push_back(&list, 10);
-    // push_back(&list, 20);
-
-    // print(&list);
-
     List* list;
-    listConstructorHeap(&list);
+    list_constructor_heap(&list);
 
-    push_back(list, 6);
-    push_back(list, 10);
-    push_back(list, 20);
-    push_front(list, 20);
-    push_front(list, 30);
-    push_front(list, 40);
-    push_at(list, 1, 100);
+    int choice, temp;
+    int tempInd;
+    char bIsRunning = 1;
+    while(bIsRunning) {
+        printf("Enter a operation:\n");
+        printf("0.Exit\n1.Display\n2.Push Front\n3.Push Back\n4.Push at any location\n5.Pop Front\n6.Pop Back\n7.Pop at any location\n");
 
-    print(list);
+        scanf("%d", &choice);
 
-    pop_front(list);
-    pop_front(list);
-    pop_front(list);
-    
-    print(list);
-
-    pop_back(list);
-    pop_back(list);
-
-    print(list);
-
-    push_back(list, 6);
-    push_back(list, 10);
-
-    print(list);
-
-    push_front(list, 20);
-    push_front(list, 30);
-
-    print(list);
-
-    pop_at(list, 0);
-    pop_at(list, 2);
-
-    print(list);
+        switch (choice){
+        case 0:
+            bIsRunning = 0;
+            break;
+        case 1:
+            printf("value: ");
+            scanf("%d", &temp);
+            list_push_front(list, temp);
+            break;
+        case 2:
+            printf("value: ");
+            scanf("%d", &temp);
+            list_push_back(list, temp);
+            break;
+        case 3:
+            printf("index: ");
+            scanf("%d", &tempInd);
+            printf("value: ");
+            scanf("%d", &temp);
+            list_push_at(list, tempInd, temp);
+            break;
+        case 4:
+            list_pop_front(list);
+            break;
+        case 5:
+            list_pop_back(list);
+            break;
+        case 6:
+            printf("index: ");
+            scanf("%d", &tempInd);
+            list_pop_at(list, tempInd);
+            break;
+        case 7:
+            list_print(list);
+            break;
+        default:
+            printf("Enter a correct choice, lol\n");
+            break;
+        }
+    }
 
     return 0;
 }
